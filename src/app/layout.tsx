7af010303464ab41b2c709278886c2fe05181b25
ბესო <besomeskhia40k@gmail.com>
2025-10-24 19:49:30 +0400

    metadata changes
    changed website name and added img to the google tab along with other metadata entries

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,46 @@ import "./globals.css";
 import ClientLayout from "./ClientLayout";
 
 export const metadata = {
-  title: "XYZ University Demo",
-  description: "Demo university website using Next.js and Tailwind",
+  title: "KIU Website Demo",
+  description:
+    "A demonstration website for Kutaisi International University (KIU), built using Next.js, React, and Tailwind CSS. Explore modern university web design concepts.",
+
+  icons: {
+    icon: "./kiuLogo.png",
+  },
+
+  openGraph: {
+    title: "Kutaisi International University (KIU) Demo",
+    description:
+      "Explore the modern web presence of KIU with this Next.js and Tailwind CSS demo.",
+    url: "https://kiu-demo-url.com",
+    siteName: "KIU Demo Site",
+    images: [
+      {
+        url: "../public/kiuLogo.png",
+        width: 1200,
+        height: 630,
+        alt: "Kutaisi International University main campus building",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+
+  keywords: [
+    "KIU",
+    "Kutaisi International University",
+    "University Demo",
+    "Next.js",
+    "Tailwind CSS",
+    "React",
+  ],
+  authors: [
+    { name: "Besik Meskhia" },
+    { name: "Nikoloz Kvirikashvili" },
+    { name: "Nikoloz Modebadze" },
+    { name: "Nika Geladze" },
+  ],
 };
 
 export default function RootLayout({
